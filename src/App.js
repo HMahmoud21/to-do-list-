4042ea0778a97d322ffaf94c1ccdd9a3ba02a916
HMahmoud21 <hajermahmoud52@gmail.com>
2022-11-15 20:46:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import TodoAdd from './components/TodoAdd';
+import TodoFilter from './components/TodoFilter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App-header">
+      <h1>Bonjour ton liste est prete </h1>
+      <TodoAdd />
+      <TodoFilter />
+      <TodoList />
     </div>
+    
   );
+  
 }
 
 export default App;
